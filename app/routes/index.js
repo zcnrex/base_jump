aeb5b5aea25b78df511a8f9b756d80428738450c
Rex <zcnrex@gmail.com>
2016-03-29 21:30:51 +0000

    add /api/whoami route

--- app/routes/index.js
+++ app/routes/index.js
@@ -55,6 +55,17 @@ module.exports = function (app, passport) {
 		.get(isLoggedIn, function (req, res) {
 			res.sendFile(path + '/public/profile.html');
 		});
+		
+	app.route('/api/whoami')
+		.get(function (req, res) {
+			var result = {
+				'ipaddress': req.headers['x-forwarded-for'],
+				'language': req.headers["accept-language"].split(',')[0],
+				'software': req.headers['user-agent'].split('(')[1].split(')')[0]
+			};
+			// console.log(result);
+			res.end(JSON.stringify(result));
+		});
 
 	app.route('/api/:id')
 		.get(isLoggedIn, function (req, res) {
